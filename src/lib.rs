1403a16d0af067f2c157d57902492a978ddb6e2d
TheRawMeatball <therawmeatball@gmail.com>
2021-03-14 18:35:12 +0300

    Fix miri

--- src/lib.rs
+++ src/lib.rs
@@ -1,91 +1,67 @@
-use std::{cell::{RefCell, Cell}, fmt::Debug, mem::MaybeUninit};
+use std::{cell::UnsafeCell, fmt::Debug};
 
-pub struct AppendVec<T, const N: usize> {
-    backing: RefCell<Vec<Box<[MaybeUninit<T>; N]>>>,
-    len: Cell<usize>,
-}
+pub struct AppendVec<T>(UnsafeCell<Vec<Box<T>>>);
 
-impl<T, const N: usize> AppendVec<T, N> {
+impl<T> AppendVec<T> {
     pub fn push(&self, t: T) {
-        let len = self.len.get();
-        if len % N == 0 {
-            self.backing
-                .borrow_mut()
-                // Safe: [MaybeUninit<T>; N] can be assumed to be initialized without any initialization
-                // https://doc.rust-lang.org/nomicon/unchecked-uninit.html
-                .push(Box::new(unsafe { MaybeUninit::uninit().assume_init() }))
-        }
-
         unsafe {
-            self.backing
-                .borrow_mut()
-                .last_mut()
-                .unwrap()
-                .get_mut(len % N)
-                .unwrap()
-                .as_mut_ptr()
-                .write(t);
+            (*self.0.get()).push(Box::new(t));
         }
-        self.len.set(len - 1);
     }
 
-    pub fn get(&self, i: usize) -> Option<&T> {
+    pub fn get<'a>(&'a self, i: usize) -> Option<&'a T> {
         unsafe {
-            (i < self.len.get())
-                .then(|| &*self.backing.borrow().get(i / N).as_ref().unwrap()[i % N].as_ptr())
+            (*self.0.get())
+                .get(i)
+                .map(|v| &**v)
+                .map(|v| std::mem::transmute::<&T, &'a T>(v))
         }
+    }
+
+    pub fn last(&self) -> Option<&T> {
+        self.get(self.len() - 1)
+    }
+
+    pub fn len(&self) -> usize {
+        unsafe { (*self.0.get()).len() }
+    }
+
+    pub fn inner(&mut self) -> &mut Vec<Box<T>> {
+        self.0.get_mut()
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        if *self.len.get_mut() == 0 {
-            return None;
-        }
-        *self.len.get_mut() -= 1;
-        let i = *self.len.get_mut();
-
-        if i % N == 0 {
-            let array = self.backing.get_mut().pop().unwrap();
-            Some(unsafe { array[0].as_ptr().read() })
-        } else {
-            let array = self.backing.get_mut().last_mut().unwrap();
-            Some(unsafe { array[i % N].as_ptr().read() })
-        }
+        self.0.get_mut().pop().map(|v| *v)
     }
 
     pub fn new() -> Self {
-        assert!(N > 0);
-        Self {
-            backing: RefCell::new(Vec::new()),
-            len: Cell::new(0),
-        }
+        Self(Default::default())
     }
 }
 
-impl<T, const N: usize> Drop for AppendVec<T, N> {
+impl<T> Drop for AppendVec<T> {
     fn drop(&mut self) {
         while let Some(_) = self.pop() {}
     }
 }
 
-impl<T: Debug, const N: usize> Debug for AppendVec<T, N> {
+impl<T: Debug> Debug for AppendVec<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut list = f.debug_list();
-        for i in 0..self.len.get() {
-            list.entry(self.get(i).unwrap());
-        }
-        list.finish()
+        self.0.fmt(f)
     }
 }
 
 #[test]
 fn test() {
-    let mut vec = AppendVec::<String, 2>::new();
+    let vec = AppendVec::<String>::new();
     println!("{:?}", &vec);
     vec.push("hello".into());
     vec.push("hi".into());
-    vec.push("filler".into());
-    vec.push("is this burning yet?".into());
+    let x = vec.get(0);
     println!("{:?}", &vec);
+    vec.push("is this burning yet?".into());
+    dbg!(x);
+    let mut vec = vec;
     println!("{:?}", vec.pop());
 
     println!("{:?}", &vec);
